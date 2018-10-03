11ba1d0c0db7e63d9ff737e1dc548c7a773e65e4
Arjun Kalburgi <askalburgi@gmail.com>
2018-10-03 17:45:23 -0400

    restaurant and selected css refactor

--- src/app/selected-restaurant/selected-restaurant.component.ts
+++ src/app/selected-restaurant/selected-restaurant.component.ts
@@ -4,8 +4,8 @@ import { Restaurant } from './../restaurant';
 @Component({
   selector: 'app-selected-restaurant',
   templateUrl: './selected-restaurant.component.html',
-  styleUrls: ['./selected-restaurant.component.css',
-              './../css/listsection.css']
+  styleUrls: ['./../restaurants/restaurants.component.css',
+              './selected-restaurant.component.css']
 })
 export class SelectedRestaurantComponent implements OnInit {
 
